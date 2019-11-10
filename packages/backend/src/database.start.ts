090454cfc2b58ea8f6c5cc1769f8b73cfa90f271
Thiago Cortez <thiago.cortez81@gmail.com>
2019-11-10 01:44:01 -0200

    mongo Producao

--- packages/backend/src/database.start.ts
+++ packages/backend/src/database.start.ts
@@ -3,6 +3,12 @@ import {Mongoose} from "mongoose";
 const mongoose = require('mongoose');
 
 export class MongoClient {
-    static mongoConnection: Mongoose = mongoose.connect(`mongodb://${config.databaseConf.mongo.host}/${config.databaseConf.mongo.database}`, {useNewUrlParser: true});
+    // static mongoConnection: Mongoose = mongoose.connect(`mongodb://${config.databaseConf.mongo.host}/${config.databaseConf.mongo.database}`, {useNewUrlParser: true});
+    static mongoConnection: Mongoose = mongoose.connect(`mongodb://${config.databaseConf.mongo.host}/${config.databaseConf.mongo.database}`, {
+        useNewUrlParser: true,
+        "auth": { "authSource": "admin" },
+        "user": "admin",
+        "pass": "abc123"
+    });
 }
 
